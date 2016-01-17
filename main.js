a57e606cde40c9b50420b40577e5d1f6692ff58f
parakram <appu226@yahoo.co.in>
2016-01-17 20:57:55 +0530

    Code formatting. Attempt at fixing error: creepTypes not defined.

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var myUtils = require('myUtils');
-module.exports.loop = function () {
-    for(var spawnId in Game.spawns) {
-	var spawn = Game.spawns[spawnId];
-	var spawnType = require(myUtils.getOrSet(spawn, 'spawnType', 'startupSpawn'));
-	spawnType.run(spawn);
-    }
-    for(var creepId in Game.creeps) {
-	var creep = Game.creeps[creepId];
-	var creepType = require(creep.creepType);
-	creepType.run(creep);
-    }
+module.exports.loop = function() {
+  for ( var spawnId in Game.spawns) {
+    var spawn = Game.spawns[spawnId];
+    var spawnType = require(myUtils
+        .getOrSet(spawn, 'spawnType', 'startupSpawn'));
+    spawnType.run(spawn);
+  }
+  for ( var creepId in Game.creeps) {
+    var creep = Game.creeps[creepId];
+    var creepType = require(creep.creepType);
+    creepType.run(creep);
+  }
 };
--- myUtils.js
+++ myUtils.js
@@ -1,29 +1,29 @@
 module.exports = {
-    getOrSet : function(object, propName, defaultValue) {
-	if (propName in object) {
-	    return object[propName];
-	} else {
-	    object[propName] = defaultValue;
-	    return defaultValue;
-	}
-    },
-    creepMoveStatic : function(creep, pos) {
-	creep.moveTo(pos, {
-	    reusePath : true,
-	    serializeMemory : 10,
-	    noPathFinding : false
-	});
-    },
-    creepHarvest : function(creep) {
-	if (!("sourceId" in creep.memory)
-		|| Game.getObjectById(creep.sourceId).energy == 0) {
-	    var activeSources = creep.room.find(FIND_SOURCES_ACTIVE);
-	    if (activeSources.size == 0)
-		return;
-	    creep.memory.sourceId = activeSources[0].id;
-	}
-	var source = Game.getObjectById(creep.memory.sourceId);
-	if (creep.harvest(source) == ERR_NOT_IN_RANGE)
-	    myUtils.creepMoveStatic(creep, source);
+  getOrSet : function(object, propName, defaultValue) {
+    if (propName in object) {
+      return object[propName];
+    } else {
+      object[propName] = defaultValue;
+      return defaultValue;
     }
+  },
+  creepMoveStatic : function(creep, pos) {
+    creep.moveTo(pos, {
+      reusePath : true,
+      serializeMemory : 10,
+      noPathFinding : false
+    });
+  },
+  creepHarvest : function(creep) {
+    if (!("sourceId" in creep.memory)
+        || Game.getObjectById(creep.sourceId).energy == 0) {
+      var activeSources = creep.room.find(FIND_SOURCES_ACTIVE);
+      if (activeSources.size == 0)
+        return;
+      creep.memory.sourceId = activeSources[0].id;
+    }
+    var source = Game.getObjectById(creep.memory.sourceId);
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE)
+      myUtils.creepMoveStatic(creep, source);
+  }
 };
