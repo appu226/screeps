281213965f069abf9b31607c9c8a272fe46474a5
parakram <appu226@yahoo.co.in>
2016-01-23 17:36:57 +0530

    Bug fixes, start off y creating 3 SlowHarvesters.

--- main.js
+++ main.js
@@ -1,129 +1,154 @@
-var MyDataStructures = {
-  queue : function() {
-    var newQueue = {
+var myDebug = function(msg) {
+  console.log(msg);
+};
+
+var MyQueue = {
+  newQueue : function() {
+    return {
       pushArray : [],
-      popArray : [],
-      push : function(obj) {
-        this.pushArray.push(obj);
-      },
-      transfer: function() {
-        if (this.popArray.length == 0) {
-          this.pushArray.reverse();
-          this.popArray = pushArray;
-          this.pushArray = [];
-        }
-      },
-      pop : function() {
-        this.transfer();
-        if (this.popArray.length == 0)
-          return null;
-        else
-          return this.popArray.pop();
-      },
-      top : function() {
-        this.transfer();
-        if (this.popArray.length == 0)
-          return null;
-        else
-          return this.popArray[this.popArray.length - 1];
-      }
+      popArray : []
     };
-    return newQueue;
+  },
+  transfer : function(queue) {
+    if (queue.popArray.length == 0) {
+      queue.pushArray.reverse();
+      queue.popArray = queue.pushArray;
+      queue.pushArray = [];
+    }
+  },
+  pop : function(queue) {
+    this.transfer(queue);
+    if (queue.popArray.length == 0)
+      return null;
+    else {
+    //   myDebug("MyQueue.pop old size is " + queue.popArray.length);
+      var result = queue.popArray.pop();
+    //   myDebug("MyQueue.pop new size is " + queue.popArray.length);
+      return result;
+    }
+  },
+  top : function(queue) {
+    this.transfer(queue);
+    if (queue.popArray.length == 0)
+      return null;
+    else
+      return queue.popArray[queue.popArray.length - 1];
+  },
+  push : function(queue, item) {
+    queue.pushArray.push(item);
   }
 };
 
 var CSlowHarvester = {
-    createBody: function(energy){
-      var body = [MOVE,CARRY,WORK];
-      energy = energy - 200;
-      while(energy >= 100) {
-        body.push(work);
-        energy = energy - 100;
-      }
-      body.reverse();
-      return body;
-    },
-    minEnergy : 200,
-    createMemory: function(spawn) {
-      return {
-        spawnId: spawn.id, 
-        defaultBehavior: {
-          behaviorType : "Harvester", 
-          args: [spawn.pos.findClosestByPath(FIND_SOURCES).id, spawn.id]
-        },
-        behaviorOverride: null
-      };
+  createBody : function(energy) {
+    myDebug("CSlowHarvester.createBody");
+    var body = [ MOVE, CARRY, WORK, WORK ];
+    energy = energy - 300;
+    while (energy >= 100) {
+      body.push(WORK);
+      energy = energy - 100;
     }
+    body.reverse();
+    return body;
+  },
+  minEnergy : 300,
+  createMemory : function(spawn) {
+    return {
+      spawnId : spawn.id,
+      defaultBehavior : {
+        behaviorType : "Harvester",
+        args : [ spawn.pos.findClosestByPath(FIND_SOURCES).id, spawn.id ]
+      },
+      behaviorOverride : null
+    };
+  }
 };
 
 var BHarvester = {
-    operate: function(creep, args) {
-      var fromId = args[0], toId = args[1];
-      var from = Game.getObjectById(fromId);
-      var to = Game.getObjectById(toId);
-      if(creep.carry.energy <= creep.carryCapacity) {
-        if(creep.harvest(from) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(from);
-          creep.transfer(to, RESOURCE_ENERGY);
-        }
-      } else if (creep.transfer(to, RESOURCE_ENERGY)) {
-        creep.moveTo(to);
-        creep.harvest(from);
+  operate : function(creep, args) {
+    var fromId = args[0], toId = args[1];
+    var from = Game.getObjectById(fromId);
+    var to = Game.getObjectById(toId);
+    if (creep.carry.energy < creep.carryCapacity) {
+      if (creep.harvest(from) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(from);
+        creep.transfer(to, RESOURCE_ENERGY);
       }
+    } else if (creep.transfer(to, RESOURCE_ENERGY)) {
+      creep.moveTo(to);
+      creep.harvest(from);
     }
+  }
 };
 
-var behaviorMap =
-  new Map(
-      [("Havester", BHarvester)]
-  );
+var behaviorMap = {
+  Harvester : BHarvester
+};
 
-var creepTypeMap = 
-  new Map(
-      [("SlowHarvester", CSlowHarvester)]
-  );
+var creepTypeMap = {
+  SlowHarvester : CSlowHarvester
+};
 
 var CreepAlgos = {
-  work: function(creep) {
+  work : function(creep) {
     var behavior = creep.memory.behaviorOverride;
-    if(behavior == null) creep.memory.defaultBehavior;
-    if(behavior != null)
+    if (behavior == null)
+      behavior = creep.memory.defaultBehavior;
+    if (behavior != null)
       behaviorMap[behavior.behaviorType].operate(creep, behavior.args);
-  }  
+  }
 };
 
 var SpawnAlgos = {
   initBuildQueue : function(spawn) {
-    if (typeof spawn.memory.buildQueue === "undefined")
-      spawn.memory.buildQueue = MyDataStructures.queue();
+    if (typeof spawn.memory.buildQueue === "undefined") {
+      var newQueue = MyQueue.newQueue();
+      for ( var i = 0; i < 3; ++i)
+        MyQueue.push(newQueue, "SlowHarvester");
+      spawn.memory.buildQueue = newQueue;
+    }
+    var nextToBuild = spawn.memory.nextToBuild;
+    if (typeof nextToBuild !== "undefined" && nextToBuild.length > 0) {
+      MyQueue.push(spawn.memory.buildQueue, spawn.memory.nextToBuild);
+    }
+    spawn.memory.nextToBuild = "";
   },
   createFromBuildQueue : function(spawn) {
-    this.initBuildQueue();
-    if( typeof processOrder(spawn, spawn.memory.buildQueue.top()) === "String" )
-      spawn.memory.buildQueue.pop();
+    myDebug("SpawnAlgos.createFromBuildQueue");
+    this.initBuildQueue(spawn);
+    if (_.isString(this.processOrder(spawn, MyQueue
+        .top(spawn.memory.buildQueue))))
+      MyQueue.pop(spawn.memory.buildQueue);
   },
   pushToBuildQueue : function(spawn, order) {
     this.initBuildQueue();
-    return spawn.memory.buildQueue.push(order);
+    return myQueue.push(spawn.memory.buildQueue, order);
   },
   processOrder : function(spawn, order) {
-    var creepType = creepMap[order.creepType];
-    if(spawn.energy < creepType.minEnergy)
+    myDebug("SpawnAlgos.processOrder( " + spawn.id + ", " + order + ")");
+    if (order == null) {
+      return null;
+    }
+    var creepType = creepTypeMap[order];
+    if (spawn.energy < creepType.minEnergy)
       return ERR_NOT_ENOUGH_ENERGY;
     var body = creepType.createBody(spawn.energy);
     var memory = creepType.createMemory(spawn);
     memory.creepType = order.creepType;
-    for(memField in order.memory)
+    for (memField in order.memory)
       memory[memField] = order.memory[memField];
     return spawn.createCreep(body, null, memory);
   }
 };
 
 module.exports.loop = function() {
-  for(spawnId in Game.spawns) {
+  myDebug("Main");
+  for ( var spawnId in Game.spawns) {
     SpawnAlgos.createFromBuildQueue(Game.spawns[spawnId]);
-  };
-  for(creepId in Game.creeps) {
+  }
+  ;
+  for ( var creepId in Game.creeps) {
     CreepAlgos.work(Game.creeps[creepId]);
-  };
+  }
+  ;
 };
